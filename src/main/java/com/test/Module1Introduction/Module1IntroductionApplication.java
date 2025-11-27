a178f700a9b671ef1021b1c84c6f22ec6ce86341
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-11-27 16:32:15 +0530

    Learning the Bean concept

--- src/main/java/com/test/Module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/test/Module1Introduction/Module1IntroductionApplication.java
@@ -1,13 +1,36 @@
 package com.test.Module1Introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module1IntroductionApplication {
+public class Module1IntroductionApplication implements CommandLineRunner {
+
+	@Autowired
+	PaymentService paymentService;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		paymentService.pay();
+
+	}
 
 	public static void main(String[] args) {
+
+		 PaymentService paymentService;
+
+
 		SpringApplication.run(Module1IntroductionApplication.class, args);
+
+		System.out.println("Appliction started successfully.");
+
+//		PaymentService paymentService = new PaymentService();  tight coupling
+
+
 	}
+
 
 }
---
+++ src/main/java/com/test/Module1Introduction/PaymentService.java
@@ -0,0 +1,12 @@
+package com.test.Module1Introduction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    void pay(){
+        System.out.println("Payment processed successfully.");
+
+    }
+}
