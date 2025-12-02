6a75bb5a5a1be58cd80836b2a0924947559db0d3
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-02 11:55:48 +0530

    Use of ConditionalOnProperty for creation of beans using conditions

--- src/main/java/com/test/Module1Introduction/impl/EmailNotificationService.java
+++ src/main/java/com/test/Module1Introduction/impl/EmailNotificationService.java
@@ -2,12 +2,13 @@ package com.test.Module1Introduction.impl;
 
 import com.test.Module1Introduction.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 //@Primary // This annotation is used to run this class when multiple implementations are present
 @Component// This annotation will create a bean of this class
-@Qualifier("email")
+@ConditionalOnProperty(name = "notification.type", havingValue="email")
 public class EmailNotificationService implements NotificationService {
     @Override
     public void send(String message) {
